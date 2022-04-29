0334d7860ed549db46a588913ebad44fc99dc7c9
Volodymyr Pysarenko <zmiyov88@gmail.com>
2022-04-29 20:56:27 +0300

    Initial Commit

---
+++ Lewowica/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lewowica
+//
+//  Created by Vladimir Pisarenko on 29.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
