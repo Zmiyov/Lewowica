da4feb509858c12cb6b28bd8164a92e1c4c00516
Volodymyr Pysarenko <zmiyov88@gmail.com>
2022-05-06 22:24:46 +0300

    Added words input and saving in dictionary

--- Lewowica/ViewController.swift
+++ Lewowica/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var words: [String: String] = [:]
 
+    @IBOutlet var addWord: UITextField!
+    @IBOutlet var addTranslate: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func addWords() {
+        if let enteredWord = addWord.text {
+            if let enteredTranslate = addTranslate.text {
+                words.updateValue(enteredTranslate, forKey: enteredWord)
+            }
+        }
+        
+    }
 
+    @IBAction func addButtonTapped(_ sender: Any) {
+        addWords()
+        addWord.text?.removeAll()
+        addTranslate.text?.removeAll()
+        print(words)
+    }
 }
 
